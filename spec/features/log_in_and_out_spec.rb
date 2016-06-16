632fc562c58587f64590299275c09cfdcb6e9271
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-06-17 00:28:42 +0200

    implement basic user login/logout

---
+++ spec/features/log_in_and_out_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper.rb'
+
+feature 'Log ins' do
+  background do
+    user = create(:user)
+  end
+
+  scenario 'user can log in' do
+    visit '/'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_content("Signed in successfully.")
+  end
+
+  scenario 'user can log out' do
+    visit '/'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+    click_link 'logout'
+    expect(page).to have_content("Signed out successfully.")
+    # expect page to have log in link
+    # expect attempt to visit account info to redirect to login page
+  end
+end
