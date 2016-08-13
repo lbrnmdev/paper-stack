7ca74870d27701df846f47f24c8db2148c9e9184
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-08-13 20:30:41 +0200

    initial deposit spec

---
+++ spec/features/deposit_amount_in_account_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper.rb'
+
+feature 'deposit amount in account' do
+  background do
+    user = create(:user_with_account)
+    login_as(user, :scope => :user)
+  end
+
+  scenario 'with valid amount' do
+    visit '/'
+    click_link 'example account'
+    click_link 'Deposit funds'
+    fill_in 'amount', with: 10.00
+    click_button 'Deposit'
+    expect(page).to have_content('€20.00')
+    expect(Account.find_by(:name => 'example account')).to be == 20.00
+  end
+end
