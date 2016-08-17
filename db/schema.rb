5458496b96c1ea8f8e4ecf9cd31cce8794a1f47b
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-08-18 00:58:08 +0200

    Revert "add transaction type enum to model, migrate"
    This reverts commit fda5c8a01c6fc970b7a024aee3e2824a95814393.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817112840) do
+ActiveRecord::Schema.define(version: 20160802144819) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -22,17 +22,6 @@ ActiveRecord::Schema.define(version: 20160817112840) do
   end
 
   add_index "accounts", ["user_id"], name: "index_accounts_on_user_id"
-
-  create_table "transactions", force: :cascade do |t|
-    t.string   "description"
-    t.decimal  "amount"
-    t.integer  "type"
-    t.integer  "account_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "transactions", ["account_id"], name: "index_transactions_on_account_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
