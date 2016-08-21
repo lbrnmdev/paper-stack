e3630331079040ca8a663016a052bc5f8c361072
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-08-22 00:38:21 +0200

    transaction updates account balance on creation

--- app/models/transaktion.rb
+++ app/models/transaktion.rb
@@ -1,4 +1,22 @@
 class Transaktion < ActiveRecord::Base
   belongs_to :account
   enum transaktion_type: [:deposit, :withdrawal]
+  after_create :insert_transaktion
+
+  private
+
+    def insert_transaktion
+      if self.deposit?
+        raise ActiveRecord::Rollback unless self.account.increment!(:balance, self.amount)
+      end
+      if self.withdrawal?
+        raise ActiveRecord::Rollback unless self.account.decrement!(:balance, self.amount)
+      end
+    end
+
+    def update_transaktion
+    end
+
+    def destroy_transaktion
+    end
 end
