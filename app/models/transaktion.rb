07769ae2ae17c23729c9737d852edaf4e085ac3d
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-08-23 09:51:04 +0200

    transaction updates account balance on destruction

--- app/models/transaktion.rb
+++ app/models/transaktion.rb
@@ -2,6 +2,7 @@ class Transaktion < ActiveRecord::Base
   belongs_to :account
   enum transaktion_type: [:deposit, :withdrawal]
   after_create :insert_transaktion
+  before_destroy :destroy_transaktion
 
   private
 
@@ -17,6 +18,17 @@ class Transaktion < ActiveRecord::Base
     def update_transaktion
     end
 
+    # what if destroy action fails? i.e transaction on account successfully reversed but Transaktion isn't deleted?
     def destroy_transaktion
+      unless self.destroyed?
+        if self.deposit?
+          # reverse
+          raise ActiveRecord::Rollback unless self.account.decrement!(:balance, self.amount)
+        end
+        if self.withdrawal?
+          # reverse
+          raise ActiveRecord::Rollback unless self.account.increment!(:balance, self.amount)
+        end
+      end
     end
 end
