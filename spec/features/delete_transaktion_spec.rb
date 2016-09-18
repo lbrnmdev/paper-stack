983796282e1f489a0244402f21ab6fa131de52d5
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-09-18 18:14:55 +0200

    style transaktions/show

--- spec/features/delete_transaktion_spec.rb
+++ spec/features/delete_transaktion_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
 
-feature 'withdraw amount from account' do
+feature 'delete transaktion' do
   background do
     user = create(:user_with_transaktion)
     login_as(user, :scope => :user)
@@ -11,8 +11,8 @@ feature 'withdraw amount from account' do
     click_link 'example account'
     click_link 'deposit of five'
     # click_link 'Thu, 31 Oct 2002 00:02:02 UTC +00:00'
-    expect(page).to have_content('Current balance: €15.00')
-    expect(page).to have_content('Transaction amount: €5.00')
+    expect(page).to have_content('Current balance is €15.00')
+    expect(page).to have_content('Amount: €5.00')
     click_link 'Delete'
     expect(page).to have_content('Transaction deleted!')
     expect(page).to_not have_content('deposit of five')
