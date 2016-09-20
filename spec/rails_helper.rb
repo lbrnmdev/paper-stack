582b10212954ce74e18601511e4740a7f7fbac01
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-09-21 00:58:52 +0200

    add devise helpers for controller testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'devise'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -59,6 +59,8 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   # setup to use devise helpers in tests
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   # https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
   config.include Warden::Test::Helpers
   config.before :suite do
