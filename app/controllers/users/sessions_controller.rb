785e73a05780147cc73a9db3a39d429a56724ef5
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-09-25 23:32:18 +0200

    finish helper to get time zone, show appropriate time in account show

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
-
+  helper SessionsHelper
   after_action :set_login_time_zone, only: [:create]
 
-  helper UsersHelper
+
 
   # GET /resource/sign_in
   # def new
@@ -27,8 +27,10 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  protected
+  private
 
+    # set time zone for current session
     def set_login_time_zone
+      session[:login_time_zone] = view_context.time_zone_from_ip(current_user.current_sign_in_ip)
     end
 end
