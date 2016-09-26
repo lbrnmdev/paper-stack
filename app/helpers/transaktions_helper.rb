5c60f583bacae8f5fc2000be214a80c690c02328
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-09-26 17:41:42 +0200

    try to return to state before time zone functionality manually

--- app/helpers/transaktions_helper.rb
+++ app/helpers/transaktions_helper.rb
@@ -1,11 +1,2 @@
 module TransaktionsHelper
-  def transaktion_date_and_time date_time
-    zoned_time = date_time
-    begin
-      zoned_time = date_time.in_time_zone(session[:login_time_zone])
-    rescue => e
-      logger.error "Error rendering date time in correct time zone: #{e.message}"
-    end
-    zoned_time.strftime("%a, %b %d, %Y at %I:%M:%S%p %Z")
-  end
 end
