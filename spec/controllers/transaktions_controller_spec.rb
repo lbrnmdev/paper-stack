6419610abf0c722f5b1cf620a9c598afed46fdac
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-09-28 23:39:19 +0200

    test transaktions#show

--- spec/controllers/transaktions_controller_spec.rb
+++ spec/controllers/transaktions_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe TransaktionsController, type: :controller do
+  # create users, accounts and transaktions for testing
+  let(:user) {create(:user)}
+  let(:other_user) {create(:user, email: "other_user@example.com")}
+  let(:account) {create(:account, user: user)}
+  let(:other_users_account) {create(:account, user:other_user)}
+  let(:transaktion) {create(:transaktion, account: account)}
+  let(:other_users_transaktion) {create(:transaktion, account: other_users_account)}
+
+  describe "GET #show" do
+    context "while logged out" do
+      it "returns http redirect" do
+        get :show, id: transaktion
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+
+    context "while logged in" do
+      before do
+        sign_in user
+      end
+
+      it "returns http redirect if not owner user" do
+        get :show, id: other_users_transaktion
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it "returns http success" do
+        get :show, id: transaktion
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
 
 end
